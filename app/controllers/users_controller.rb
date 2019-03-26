8d80ae2e5d1f2e74c9912e7123b33a92097cdab4
mcrd25 <mayacrd25@gmail.com>
2019-03-26 17:15:06 -0400

    Made change based on exercise showing first post if it exists as it was giving an error if post did not already exist and fixed the user variable error by passing param id to get from url

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,8 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
-    @user = User.first
-    @micropost = @user.microposts.first 
+    @user = User.find(params[:id])
+    @first_micropost = @user.microposts.first 
   end
 
   # GET /users/new
